64015d0cfec3a086c356b40cea194d0544276b73
AnpanMan17 <laurasurcoenzo@gmail.com>
2025-11-20 07:38:14 -0300

    Mejora de comentarios en script.js para mayor claridad en las etapas de carga

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ import { sleep, check } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 200 },   // sanity
-    { duration: '30s', target: 500 },   // comprobar 500
-    { duration: '30s', target: 1000 },  // comprobar 1k
-    { duration: '30s', target: 2000 },  // comprobar 2k
+    { duration: '30s', target: 200 },   // 200 usuarios
+    { duration: '30s', target: 500 },   // 500 usuarios
+    { duration: '30s', target: 1000 },  // 1k usuarios
+    { duration: '30s', target: 2000 },  // 2k usuarios
     { duration: '30s', target: 0 },     // cool-down
   ],
   thresholds: {
-    'http_req_failed': ['rate<0.05'],   // fail si >5% fallan
-    'http_req_duration': ['p(95)<2000'],// fail si p95 > 2000ms
+    'http_req_failed': ['rate<0.05'],     // fail si >5% fallan
+    'http_req_duration': ['p(95)<2000'],  // fail si p95 > 2000ms
   },
 };
 
